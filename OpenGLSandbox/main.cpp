002c1b73d5cde61b1f911f632f9e29037de2f0d2
Soham Ghugare <soham.ghugare@gmail.com>
2023-10-30 01:40:57 +0530

    initial commit

---
+++ OpenGLSandbox/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLSandbox
+//
+//  Created by Soham Ghugare on 30/10/23.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
