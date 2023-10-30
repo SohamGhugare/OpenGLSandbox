5aca458c98b0c7f19476f16ffeafc5f05fcd1f3d
Soham Ghugare <soham.ghugare@gmail.com>
2023-10-30 22:50:04 +0530

    feat: added window null safety

--- OpenGLSandbox/main.cpp
+++ OpenGLSandbox/main.cpp
@@ -26,4 +26,17 @@ int main() {
     
     // Setting up forward compatibility
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    // Creating a window obj
+    GLint WIDTH = 800, HEIGHT = 600;
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGLSandbox", NULL, NULL);
+    
+    if (window==NULL) {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    
+    
 }
